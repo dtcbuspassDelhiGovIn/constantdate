094db1bbd93d6718fd23e560feb169c9fa56ca41
Thakur Dutt <duttthakur444@gmail.com>
2023-04-18 19:07:17 +0530

    Update date.js

--- date.js
+++ date.js
@@ -1,4 +1,4 @@
-hit window.onload = function(){
+window.onload = function(){
   document.getElementById('start-date').innerHTML = "10/04/2023";
   document.getElementById('end-date').innerHTML = "09/07/2023";
   document.getElementById('pass-no').innerHTML = "yzx";
