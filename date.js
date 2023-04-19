7d355add24d5398a8e2e2bb0d45a3180779ce2a4
dtcbuspassDelhiGovIn <129287314+dtcbuspassDelhiGovIn@users.noreply.github.com>
2023-04-19 13:18:21 +0530

    fixing the double (//).

--- date.js
+++ date.js
@@ -1,6 +1,6 @@
 window.onload = function(){
-  document.getElementById('start-date').innerHTML = "/04/2023";    
-  document.getElementById('end-date').innerHTML = "/07/2023";
+  document.getElementById('start-date').innerHTML = "04/2023";    
+  document.getElementById('end-date').innerHTML = "07/2023";
   //both dates contains month and year only as I want to fetch the 'day' from the HTML File.
   document.getElementById('pass-no').innerHTML = "7504032396";
   //pass no contains 10-digits, last 3 digits will be fetch from HTML file.
