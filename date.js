1aa3048f20dd7360115ed648ba4f2d936e437157
dtcbuspassDelhiGovIn <129287314+dtcbuspassDelhiGovIn@users.noreply.github.com>
2023-08-12 01:38:54 +0530

    updated dates n No

--- date.js
+++ date.js
@@ -1,12 +1,12 @@
 window.onload = function(){
-  document.getElementById('start-date').innerHTML = "06/2023";    
-  document.getElementById('end-date').innerHTML = "10/2023";
+  document.getElementById('start-date').innerHTML = "07/2023";    
+  document.getElementById('end-date').innerHTML = "11/2023";
   //both dates contains month and year only as I want to fetch the 'day' from the HTML File.
 
   //I added the MMYY for buspass no, incase i need to use this file.
-  document.getElementById('month-year').innerHTML = "0623";
+  document.getElementById('month-year').innerHTML = "0723";
 
-  document.getElementById('pass-no').innerHTML = "7504";
+  document.getElementById('pass-no').innerHTML = "7505";
   //pass no contains 10-digits, last 3 digits will be fetch from HTML file.
 };
 
